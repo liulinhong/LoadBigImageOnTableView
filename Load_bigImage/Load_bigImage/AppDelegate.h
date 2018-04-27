228a74ef04efc76de9008feb1d202f91ab465d45
LiuLinhong <liulinhong@feinno.com>
2018-04-27 11:35:16 +0800

    init

---
+++ Load_bigImage/Load_bigImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Load_bigImage
+//
+//  Created by chrise on 2018/4/9.
+//  Copyright © 2018年 chrise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Load_bigImage/Load_bigImage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Load_bigImage
+//
+//  Created by chrise on 2018/4/9.
+//  Copyright © 2018年 chrise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
